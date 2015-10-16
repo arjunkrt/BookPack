fa74f9e392f8998e887b0379937d5c3f45eff554
ghanams <ghanams@ncsu.edu>
2015-10-15 23:31:28 -0400

    Added server.java.

---
+++ src/com/bookpack/jdbc/search.java
@@ -0,0 +1,28 @@
+package com.bookpack.jdbc;
+import java.util.Scanner;
+
+public class search {
+	
+	public static void search_resource(){
+		
+		System.out.println("1. Search Book. ");
+		System.out.println("2. Search Research Journal. ");
+		System.out.println("3. Search Camera. ");
+		System.out.println("4. Search Room. ");
+		
+		Scanner in = new Scanner(System.in);
+		int num = in.nextInt();
+		
+		switch(num){
+		
+		case 1:;break;
+		case 2:;break;
+		case 3:;break;
+		case 4:;break;
+		default:;break;
+		}
+	
+		
+	}
+
+}
