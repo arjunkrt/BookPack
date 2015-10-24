b98ae8c7fe05a49d759f3d431a664252ca3666e0
ArjunKarat <akarat@ncsu.edu>
2015-10-24 15:56:17 -0400

    changes wrt main and login

--- src/com/bookpack/jdbc/Login.java
+++ src/com/bookpack/jdbc/Login.java
@@ -16,6 +16,63 @@ public class Login {
 	public static Login getInstance( ) {
 		return login;
 	}
+	public void main_screen()
+	{
+		int func;
+
+		do {
+			System.out.println();
+			System.out.println("BookPack! Library Management System");
+			System.out.println("----------------------------------");
+			System.out.println("<Menu>");	
+			System.out.println("1. Login");
+			System.out.println("2. Quit");
+			System.out.print("Enter your Choice >> ");
+			func = stdin.nextInt();
+			stdin.nextLine();
+			switch (func) {
+			case 1:
+				login.login_screen();
+				if (login.success_id > 0) {
+					
+					System.out.println("----------------------------------");
+					System.out.println("<Menu>");	
+					System.out.println("1. Profile");
+					System.out.println("2. Resources");
+					System.out.println("3. Checked­out Resources");
+					System.out.println("4. Resource Request");
+					System.out.println("5. Notifications");
+					System.out.println("6. Due­Balance");
+					System.out.println("7. Logout");
+					System.out.print("Enter your Choice >> ");
+					func = stdin.nextInt();
+					stdin.nextLine();
+					switch (func) {
+					case 1:
+						Profile profile = Profile.getInstance();
+						profile.display_student(login);
+						break;
+					case 2:
+						
+						break;
+					case 3:
+						Resource_check_out resource_check_out = Resource_check_out.getInstance();
+						resource_check_out.display_checked_out_resources(login);
+						break;
+					default:
+						System.out.println("Wrong input. Try again!");
+					}		
+				}
+				break;
+			case 2:
+				System.out.println("Bye!!!!!!!!!!!!!!!!!!!!!!!");
+				break;
+			default:
+				System.out.println("Wrong input. Try again!");
+			}
+		}
+		while (func != 2);
+	}
 	public void login_screen()
 	{
 		System.out.println();
@@ -48,7 +105,6 @@ public class Login {
 
 			if (success_id == 0){
 				System.out.println("Login Failed, Please login again");
-				Login login = Login.getInstance();
 				login.login_screen();
 			}
 		}
