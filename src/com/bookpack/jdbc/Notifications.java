86317e48f762a02fe58be5f5b406f0cf1d5e8be6
ArjunKarat <arjunkrt@gmail.com>
2015-11-02 00:57:58 -0500

    checking out resources

--- src/com/bookpack/jdbc/Notifications.java
+++ src/com/bookpack/jdbc/Notifications.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 
 public class Notifications {
 
-
 	static Scanner stdin = new Scanner(System.in);
 	private static Notifications notifications = new Notifications( );
 	
@@ -19,41 +18,51 @@ public class Notifications {
 		System.out.println("Display Notifications");
 		System.out.println("----------------------------------");
 		
-		String sql = "select nt.*, np.notification_id, np.patron_id, "
-				+ "np.attribute1, "
-				+ "np.notif_seen, np.notif_sent, ngp.attribute1 as attribute1_1, ngp.attribute2, "
-				+ "ngp.attribute3 from athoma12.notification_templates nt, "
-				+ "athoma12.notification_patrons np, athoma12.notification_grp_params ngp  "
-				+ "where nt.template_name = np.template_name "
-				+ "and nt.template_name = 'FIRST_REMINDER' "
-				+ "and np.notif_seen <> 'Y' "
-				+ "and ngp.notification_id = np.notification_id "
-				+ "and np.patron_id = ?";
+		String sql1 = "select * from athoma12.notification_patrons np, "
+				+ "athoma12.notification_templates nt where np.patron_id = ? "
+				+ "and np.template_name = nt.template_name";
+		
+		String sql2 = "select * from athoma12.notification_attributes na "
+				+ "where notification_id = ? order by attribute_number";
+		
+		String sql3 = "select * from notification_grp_params ngp "
+				+ "where ngp.notification_id = ?";//1018
 		
 		PreparedStatement cstmt=null;
-		ResultSet rs = null;
-		
+		ResultSet rs1,rs2,rs3 = null;
+		String message1 = null, message2 = null, message3 = null;
+		login.patron_id = 1021;
 		try {
-			cstmt = DBConnection.conn.prepareStatement(sql);
-			cstmt.setDouble(1, login.patron_id);
+			cstmt = DBConnection.conn.prepareStatement(sql1);
+			cstmt.setDouble(1,login.patron_id);//1021
 
-			rs = cstmt.executeQuery();
+			rs1 = cstmt.executeQuery();
 				
-			System.out.print("Sl.No    ");
-			System.out.print("Resource Type    ");
-			System.out.println("Due date"); 
-			
-			while(rs.next())
+//			System.out.print("Sl.No    ");
+//			System.out.print("Resource Type    ");
+//			System.out.println("Due date"); 
+			while(rs1.next())
 			{
-				String sl_no = rs.getString("ATTRIBUTE1_1");
-				String resource_type = rs.getString("ATTRIBUTE2");
-				String date = rs.getString("ATTRIBUTE3");
-				System.out.print(sl_no);
-				System.out.print("             ");
-				System.out.print(resource_type);
-				System.out.print("             ");
-				System.out.println(date);
+				message1 = rs1.getString("TEMPLATE_BODY");
 
+				cstmt = DBConnection.conn.prepareStatement(sql2);
+				cstmt.setInt(1,1019/*login.patron_id*/);//1019
+
+				rs2 = cstmt.executeQuery();
+				while(rs2.next())
+				{
+//					if(rs2.getInt("ATTRIBUTE_NUMBER") == 1)
+//					{
+//						message2 = message1.replace("|ARG|",rs2.getString("ATTRIBUTE_VALUE"));
+//					}
+//					else if(rs2.getInt("ATTRIBUTE_NUMBER") == 2)
+//					{
+//						message2 = message2.replace("|ARG|",rs2.getString("ATTRIBUTE_VALUE"));
+//					}
+					System.out.println(rs2.getString("ATTRIBUTE_VALUE"));
+					message2 = message1.replace("|ARG|",rs2.getString("ATTRIBUTE_VALUE"));
+				}
+				System.out.println(message2);
 			}
 		}
 		catch (SQLException e) {
